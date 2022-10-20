e3bd5f617a8cff1d05a1479832a68245ee95f178
pigeon-phobia <jbq3712@gmail.com>
2022-10-20 16:14:52 +0800

    1. Update lat/lng plot axis 2. Add preprocessing steps

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -31,13 +31,13 @@ def fix_abnormal_geo_location(df):
 def save_geo_scatter_plot(lat_array, lng_array, filename):
     # Calculate the point density
     print('Stacking data...')
-    geo_coordinates = np.vstack([lat_array, lng_array])
+    geo_coordinates = np.vstack([lng_array, lat_array])
     print('Calculating kernel density...')
     density = gaussian_kde(geo_coordinates)(geo_coordinates)
 
-    plt.scatter(lat_array, lng_array, c=density)
-    plt.xlabel('Latitude')
-    plt.ylabel('Longitude')
+    plt.scatter(lng_array, lat_array, c=density)
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
     plt.savefig(filename, dpi=500)
 
 
@@ -112,6 +112,24 @@ def fillna_by_grouping(df, na_attr, grouping_attr):
     # print('still na', df[na_attr].isna().sum())
 
 
+# require size_sqft to be fixed first
+def fill_na_num_beds(df):
+    na_mask = df['num_beds'].isna()
+    sqft_mask = df['size_sqft'] < 1000.
+    # all studios, 0 would be a meaningful value
+    df.loc[na_mask & sqft_mask, 'num_beds'] = 0.
+
+    na_mask = df['num_beds'].isna()
+    # super big studio, often configured into multiple bedrooms, just follow num_baths here
+    df.loc[na_mask, 'num_beds'] = df.loc[na_mask, 'num_baths']
+
+    # manually lookup, for num_baths also
+    df.loc[3713, 'num_beds'] = 5.
+    df.loc[3713, 'num_baths'] = 5.
+    df.loc[5002, 'num_beds'] = 3.
+    df.loc[5002, 'num_baths'] = 2.
+
+
 def discretize_built_year(df, bins, labels):
     ds_interval = pd.cut(df['built_year'], bins=bins)
     ds_labels = pd.cut(ds_interval, bins).map(labels)
@@ -120,6 +138,111 @@ def discretize_built_year(df, bins, labels):
 
 def fill_conservation_house_built_year(df, value):
     df.loc[df['property_type'] == 'conservation house', 'built_year'] = value
+
+
+def fix_abnormal_beds_baths_number(df):
+    mask = (df['num_beds'] == 1.0) & (df['num_baths'] == 10.0)
+    df.loc[mask, 'num_baths'] = 1.0
+
+
+def fix_super_high_price(df):
+    # tune for each super high price
+    df.loc[635, 'price'] = df.loc[635, 'price'] / 100  # land only, previously 1e8 plus
+    df.loc[5976, 'price'] = df.loc[5976, 'price'] / 1e5  # hdb, previously 3.9e10
+    df.loc[16264, 'price'] = df.loc[16264, 'price'] / 1e4  # hdb, previously 4.9e9
+
+    # super expensive hdb
+    df.loc[3066, 'price'] = df.loc[3066, 'price'] / 5  # previously 2,100,000
+    df.loc[3430, 'price'] = df.loc[3430, 'price'] / 10  # previously 8,400,000
+    df.loc[9744, 'price'] = df.loc[9744, 'price'] / 5  # previously 1,155,000
+
+    # condo / ec
+    df.loc[663, 'price'] = df.loc[663, 'price'] / 10  # previously 30,880,500
+    df.loc[19587, 'price'] = df.loc[19587, 'price'] / 10  # previously 16,884,000
+
+
+def remove_price_zero_records(df):
+    df.drop(df[df['price'] == 0.].index, inplace=True)
+
+
+def fill_zero_sqft(df):
+    mask = (df['size_sqft'] == 0.) & (df['property_name'] == 'clavon') & (df['num_beds'] == 5.)
+    df.loc[mask, 'size_sqft'] = 1690.
+
+
+def fix_abnormal_sqft(df):
+    # obtain the insights from EDA
+    sqft_mask_1 = (df['size_sqft'] > 35000) & (df['size_sqft'] < 1e5)
+    sqft_mask_2 = (df['size_sqft'] > 1e6) & (df['size_sqft'] < 1e7)
+    df.loc[sqft_mask_1, 'size_sqft'] = df.loc[sqft_mask_1, 'size_sqft'] / 10
+    df.loc[sqft_mask_2, 'size_sqft'] = df.loc[sqft_mask_2, 'size_sqft'] / 1000
+
+    # super large hdb, ranging from 6,000 to 15,000
+    sqft_mask_3 = (df['size_sqft'] > 6000) & (df['property_type'] == 'hdb')
+    df.loc[sqft_mask_3, 'size_sqft'] = df.loc[sqft_mask_3, 'size_sqft'] / 10
+
+    # super large condo
+    df.loc[16370, 'size_sqft'] = df.loc[16370, 'size_sqft'] / 10  # previously 13,000
+
+    # abnormal landed
+    df.loc[13461, 'size_sqft'] = df.loc[13461, 'size_sqft'] / 10  # previously 25,000
+
+    # specific case
+    df.loc[5976, 'size_sqft'] = 1313  # find from other same property records
+
+def convert_sqm_to_sqft(df):
+    sqm_to_sqft_multiplier = 10.764
+    sqft_mask_sqm = (df['size_sqft'] < 200) & (df['size_sqft'] > 0)
+    df.loc[sqft_mask_sqm, 'size_sqft'] = df.loc[sqft_mask_sqm, 'size_sqft'] * sqm_to_sqft_multiplier
+
+    # cluster house
+    df.loc[14218, 'size_sqft'] = df.loc[14218, 'size_sqft'] * sqm_to_sqft_multiplier
+    df.loc[15027, 'size_sqft'] = df.loc[15027, 'size_sqft'] * sqm_to_sqft_multiplier
+
+
+def target_encode_property_type_subzone(df):
+    df_price_sqft = df.groupby(['subzone', 'property_type'])[['price', 'size_sqft']].sum().reset_index()
+    df_price_sqft['sqft_price'] = df_price_sqft['price'] / df_price_sqft['size_sqft']
+    df_price_sqft.drop(columns=['price', 'size_sqft'], inplace=True)
+    encoding_dict = df_price_sqft.set_index(['subzone', 'property_type']).to_dict()['sqft_price']
+    df['subzone_property_type_encoding'] = df.apply(lambda x: encoding_dict[(x['subzone'], x['property_type'])], axis=1)
+    df.drop(columns=['subzone', 'property_type'], inplace=True)
+
+    cols = list(df.columns)
+    a, b = cols.index('price'), cols.index('subzone_property_type_encoding')
+    cols[b], cols[a] = cols[a], cols[b]
+    df = df[cols]
+    return df, encoding_dict
+
+
+def encode_built_year(df, map_dict):
+    df['built_year'] = df['built_year'].map(map_dict)
+
+
+def encode_tenure(df):
+    tenure_dict = {
+        '99-year leasehold': 0.,
+        '999-year leasehold': 1.,
+        'freehold': 2.
+    }
+    df['tenure'] = df['tenure'].map(tenure_dict)
+
+
+def encode_planning_area(df, pa_list):
+    df_pa_list = df['planning_area'].to_list()
+    pa_dict = {k: v for v, k in enumerate(pa_list)}
+    ont_hot_length = len(pa_list)
+    one_hot_array = np.zeros((len(df_pa_list), ont_hot_length))
+    for i, pa in enumerate(df_pa_list):
+        one_hot_index = pa_dict[pa]
+        one_hot_array[i][one_hot_index] = 1.
+    pa_columns = ['pa_' + pa.replace(' ', '_') for pa in pa_list]
+    df_pa = pd.DataFrame(one_hot_array, columns=pa_columns)
+    df.reset_index(drop=True, inplace=True)
+    df_pa.reset_index(drop=True, inplace=True)
+    df = df.join(df_pa)
+    df.drop(columns=['planning_area'], inplace=True)
+    return df
 
 
 # num beds num bath
